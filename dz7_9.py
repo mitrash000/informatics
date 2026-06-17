8afa334233db5bb80bc8368642350e9ff74f48c7
mitrash000 <Lexnis@list.ru>
2026-06-17 11:42:08 +0300

    Update dz7_9.py

--- dz7_9.py
+++ dz7_9.py
@@ -1,17 +1,17 @@
 #coding=windows-1251
-def gold(n, M, z):
-    dp = [0] * (M + 1)
+def gold(M, z):
+    z.sort(reverse=True) 
+    aii_w = 0
     
-    for zs in z:
-        for w in range(M, zs - 1, -1):
-            dp[w] = max(dp[w], dp[w - zs] + zs)
-    
-    return dp[M]
+    for w in z:
+        if aii_w + w <= M:
+            aii_w += w
+    return aii_w
 
 
-n1, M1 = 5, 15
+M1 = 15
 z1 = [3, 8, 1, 2, 5]
-n2, M2 = 5, 19
+M2 = 19
 z2 = [10, 10, 7, 7, 4]
-print(gold(n1, M1, z1))  
-print(gold(n2, M2, z2))  
+print(gold(M1, z1))  
+print(gold(M2, z2))  
